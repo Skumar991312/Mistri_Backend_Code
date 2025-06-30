06d1245b4ea7ff930131a79cf74753cadda030b7
SANDEEP KUMAR <sdeepmail419@gmail.com>
2025-06-30 19:19:36 +0530

    initial commit

---
+++ src/main/java/com/mistri/MistriBApplication.java
@@ -0,0 +1,13 @@
+package com.mistri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MistriBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MistriBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mistri/MistriBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mistri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MistriBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
